daf38ce4383055f32c01c02b5f0cbd2f9f2dc68c
hal-pemulis <travisatyler@gmail.com>
2020-04-22 21:36:36 -0500

    Added new code file

--- Schema/mobility_schema.sql
+++ Schema/mobility_schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE mobility (
 	id SERIAL PRIMARY KEY,
+	date DATE NOT NULL,
 	country_name VARCHAR(50) NOT NULL,
 	apple_change FLOAT NOT NULL,
 	google_change FLOAT NOT NULL
@@ -7,6 +8,7 @@ CREATE TABLE mobility (
 
 CREATE TABLE infections (
 	id SERIAL PRIMARY KEY,
+	date DATE NOT NULL,
 	county_name VARCHAR(50) NOT NULL,
 	total_infections INT NOT NULL,
 	total_deaths INT NOT NULL
