09f9699a050cff8db3a9d7051ba74e2dd3faa75d
hal-pemulis <travisatyler@gmail.com>
2020-04-22 21:23:52 -0500

    Added initial schema query; added infection data to panads code

---
+++ Schema/mobility_schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE mobility (
+	id SERIAL PRIMARY KEY,
+	country_name VARCHAR(50) NOT NULL,
+	apple_change FLOAT NOT NULL,
+	google_change FLOAT NOT NULL
+);
+
+CREATE TABLE infections (
+	id SERIAL PRIMARY KEY,
+	county_name VARCHAR(50) NOT NULL,
+	total_infections INT NOT NULL,
+	total_deaths INT NOT NULL
+);
+
+CREATE TABLE population (
+	id SERIAL PRIMARY KEY,
+	county_name VARCHAR(50) NOT NULL,
+	population INT NOT NULL
+);
+
