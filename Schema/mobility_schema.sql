a2284a83f22fd928c9e9196c4090a02dc85da1bf
hal-pemulis <travisatyler@gmail.com>
2020-04-25 12:02:30 -0500

    Added table schema

--- Schema/mobility_schema.sql
+++ Schema/mobility_schema.sql
@@ -1,22 +1,30 @@
-CREATE TABLE mobility (
+CREATE TABLE apple_mobility (
 	id SERIAL PRIMARY KEY,
-	date DATE NOT NULL,
-	country_name VARCHAR(50) NOT NULL,
-	apple_change FLOAT NOT NULL,
-	google_change FLOAT NOT NULL
+	country VARCHAR(50) NOT NULL,
+	dates DATE NOT NULL,
+	mobility_percent_from_base FLOAT NOT NULL
+);
+
+CREATE TABLE google_mobility (
+	id SERIAL PRIMARY KEY,
+	country VARCHAR(50) NOT NULL,
+	dates DATE NOT NULL,
+	mobility_percent_from_base FLOAT NOT NULL
 );
 
 CREATE TABLE infections (
 	id SERIAL PRIMARY KEY,
-	date DATE NOT NULL,
-	county_name VARCHAR(50) NOT NULL,
-	total_infections INT NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	dates DATE NOT NULL,
+	new_cases INT NOT NULL,
+	new_deaths INT NOT NULL,
+	total_cases INT NOT NULL,
 	total_deaths INT NOT NULL
 );
 
 CREATE TABLE population (
 	id SERIAL PRIMARY KEY,
-	county_name VARCHAR(50) NOT NULL,
+	country VARCHAR(50) NOT NULL,
 	population INT NOT NULL
 );
 
