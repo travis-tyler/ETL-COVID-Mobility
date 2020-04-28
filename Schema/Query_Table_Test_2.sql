8c7e6af85f60a2e58129c6f630960f8614079a64
Robert Ottogalli <59754310+Rob-Ottogalli@users.noreply.github.com>
2020-04-27 20:51:34 -0500

    Finalized Jupyter Notebook

--- Schema/Query_Table_Test_2.sql
+++ Schema/Query_Table_Test_2.sql
@@ -1,36 +1,12 @@
-SELECT distinct country
-FROM apple_mobility;
-
-SELECT *
-FROM apple_mobility;
-
-SELECT * 
-FROM google_mobility;
-
-SELECT *
-FROM infections;
-
-SELECT *
-FROM countries;
-
-SELECT distinct c.country, a.country
-FROM countries as c
-RIGHT JOIN apple_mobility as a
-ON c.country = a.country
-ORDER BY c.country asc;
-
-SELECT distinct c.country, g.country
-FROM countries as c
-RIGHT JOIN google_mobility as g
-ON c.country = g.country
-ORDER BY c.country asc;
-
-SELECT distinct c.country, i.country
-FROM countries as c
-RIGHT JOIN infections as i
-ON c.country = i.country
-ORDER BY c.country asc;
-
--- in Apple drop "Czech Republic", "UK", "Republic of Korea"
--- in Google drop "Cape Verde", "Czechia", "Myanmar (Burma)", "The Bahamas"
--- in Infections drop "Cote d'Ivoire", "Czech Republic", "Democratic Republic of Congo", "Guernsey", "International", "Macedonia", "Saint Barthlemy", "Saint Martin (French part)", "Vatican", "Worldwide"
+-- Run a SQL query to show country name, dates, total cases, and mobility data
+SELECT 
+	agm.country, 
+	agm.dates, 
+	i.total_cases, 
+	agm.apple_mobility_percent, 
+	agm.google_mobility_percent
+FROM infections as i
+INNER JOIN apple_google_mobility as agm 
+ON i.country = agm.country
+AND i.dates = agm.dates
+ORDER BY agm.country asc;
--- Schema/apple_google_tables.sql
+++ Schema/apple_google_tables.sql
@@ -22,30 +22,7 @@ CREATE TABLE infections (
 	total_deaths INT NOT NULL
 );
 
--- CREATE TABLE apple_mobility (
--- 	id SERIAL PRIMARY KEY,
--- 	country VARCHAR(50) NOT NULL,
--- 	dates DATE NOT NULL,
--- 	country_id INT,
--- 	mobility_percent_from_base FLOAT NOT NULL
--- );
-
--- CREATE TABLE google_mobility (
--- 	id SERIAL PRIMARY KEY,
--- 	country VARCHAR(50) NOT NULL,
--- 	dates DATE NOT NULL,
--- 	mobility_percent_from_base FLOAT NOT NULL
--- );
-
-ALTER TABLE apple_google_mobility 
-	ADD CONSTRAINT country_id FOREIGN KEY (country_id) REFERENCES countries(id) MATCH FULL;
-
-
-
-DROP TABLE apple_google_mobility;
-DROP TABLE countries;
-DROP TABLE infections;
--- DROP TABLE apple_mobility;
--- DROP TABLE google_mobility;
-
+-- Attempt to create foreign key
+--ALTER TABLE apple_google_mobility 
+--	ADD CONSTRAINT country_id FOREIGN KEY (country_id) REFERENCES countries(id) MATCH FULL;
 
